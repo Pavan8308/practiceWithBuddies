c338c1d4d717c5f969e4abfe76cf0c98b540fbc7
Pavan8308 <pj00576@gmail.com>
2024-10-29 12:59:18 +0530

    Class name changes sucessfully

--- helloGuys.java
+++ helloGuys.java
@@ -1,7 +1,8 @@
-class demo
+class helloGuys
 {
 public static void main(String []args)
 {
-System.out.println("Hello Guys");
+    System.out.println("Hello Guys");
+    System.out.println("Bye Guys");
 }
 }
